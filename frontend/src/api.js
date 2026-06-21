4ce49d6db07801aea6ef4bf97f7a654ae9294fce
somu2277 <somu8297968923@gmail.com>
2026-06-21 22:07:06 +0530

    Auto-append /api to VITE_API_URL to fix 404 routing errors

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+let baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+if (baseURL && !baseURL.endsWith('/api')) {
+  baseURL = baseURL.replace(/\/+$/, '') + '/api';
+}
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL,
 });
 
 api.interceptors.request.use(
